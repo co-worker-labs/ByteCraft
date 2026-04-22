1ca4f461ba57c5c29852afb06bb24b4d9ef7c6bd
KANG <kangljchn@163.com>
2026-04-22 13:16:55 +0800

    refactor(layout): remove RECENT feature from sidebar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,10 @@
-import { CSSProperties, ReactNode, useEffect, useMemo } from "react";
+import { CSSProperties, ReactNode, useEffect } from "react";
 import Footer, { FooterPosition } from "./footer";
 import Header, { HeaderPosition } from "./header";
 import { Context, createContext, useContext, useState } from "react";
 import styles from "./Layout.module.css";
 import { useRouter } from "next/router";
-import { listRecents, logAccess } from "../libs/recent";
 import { pathTrim } from "../utils/path";
-import Link from "next/link";
-import { useTranslation } from "next-i18next/pages";
 
 interface LayoutSettings {
   reset: () => void;
@@ -51,12 +48,6 @@ export default function Layout({
 
   const router = useRouter();
   const path = pathTrim(router.asPath);
-  const { t } = useTranslation(["common", "tools"]);
-
-  const recent = useMemo(() => {
-    if (typeof window === "undefined") return [];
-    return listRecents([path]);
-  }, [path]);
 
   const config = {
     reset: () => {
@@ -83,7 +74,6 @@ export default function Layout({
         }
       }
     });
-    logAccess(path);
   }, [path]);
 
   return (
@@ -106,33 +96,7 @@ export default function Layout({
           {aside ? (
             <div className="row justify-content-center px-0 gx-0">
               <div className="col d-none d-lg-block">
-                <div className="w-100 row justify-content-center ps-2 mt-2">
-                  {recent.length > 0 && (
-                    <div className={`${styles.asideContent} mt-3 px-2 mb-2`}>
-                      <div className="h5 fw-bolder text-danger text-uppercase">
-                        {t("common:layout.recent")}
-                      </div>
-                      <hr />
-                      {recent.map((data, index) => {
-                        return (
-                          <div className="card mt-3 text-center" key={index}>
-                            <div className="card-body py-2">
-                              <Link href={data.path} className={`${styles.asideItem}`}>
-                                <h5 className="card-title fw-bold">{data.title}</h5>
-                                <p
-                                  className="card-text text-truncate text-wrap text-muted"
-                                  style={{ maxHeight: "2.8rem" }}
-                                >
-                                  {data.description}
-                                </p>
-                              </Link>
-                            </div>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  )}
-                </div>
+                <div className="w-100 row justify-content-center ps-2 mt-2"></div>
               </div>
               <div className={`col col-lg-7`}>{children}</div>
               <div className="col d-none d-lg-block">
