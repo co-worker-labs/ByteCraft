ac91d80238c02f04e5da52a8e4f23a026ba49d52
KANG <kangljchn@163.com>
2026-04-22 14:34:43 +0800

    refactor(app): replace bootstrap imports with tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "../styles/globals.scss";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 import { ThemeProvider } from "../libs/theme";
 import { appWithTranslation } from "next-i18next/pages";
 
 function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
   return (
     <ThemeProvider>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { ToastContainer } from "../components/toast";
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6128301546730956"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Head />
       <body>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(){try{var t=localStorage.getItem('bytecraft-theme');if(t==='dark'||t==='light'){document.documentElement.setAttribute('data-bs-theme',t)}else if(window.matchMedia('(prefers-color-scheme:dark)').matches){document.documentElement.setAttribute('data-bs-theme','dark')}}catch(e){}})()`,
+            __html: `
+              (function() {
+                var theme = localStorage.getItem('bytecraft-theme');
+                if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark');
+                }
+              })();
+            `,
           }}
         />
         <Main />
         <NextScript />
-        <ToastContainer />
       </body>
     </Html>
   );
